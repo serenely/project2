742b318a0ffe92ca0fe5a091be2f93427198ad8e
Serenely <shilovboris12@gmail.com>
2021-10-09 21:32:16 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+let answers = [],
+    questions = [
+        'Как тебя зовут ?',
+        'Склько тебе лет?',
+        'Какое твое будущее?'
+    ];
+
+
+for (let i = 0; i < questions.length; i++) {
+
+    answers[i] = prompt(questions[i], '');
+}
+
+
+
+
+
+let answers1 = [],
+    questions1 = [
+        'Как тебя зовут ?',
+        'Склько тебе лет?',
+        'Какое твое будущее?'
+    ];
+
+let i = 0;
+do {
+    console.log(answers1[i]);
+    i++;
+    answers1[i] = prompt(questions1[i]);
+
+} while (i <= questions1.length);
+
+
+
+
+
+
+let answers2 = [],
+    questions2 = [
+        'Как тебя зовут ?',
+        'Склько тебе лет?',
+        'Какое твое будущее?'
+    ];
+
+let s = 0;
+
+while (s <= questions2.length) {
+    console.log(s);
+    s++;
+    answers2[s] = prompt(questions2[i]);
+
+}
+
+
+
+
+
+
